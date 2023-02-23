3681d3acfbcebc0c4c5c63181b012c7a6aa45a4d
neharahman17 <neharahman877@gmail.com>
2023-02-23 22:07:52 +0530

    add referAndEarn

--- controller/referAndEarn.js
+++ controller/referAndEarn.js
@@ -1,28 +1,23 @@
 const SignupModel = require('../model/signup_model.js');
 const {verifyToken} = require('../other/jwtToken.js')
 
-module.exports.myRewards=async (req,res)=>{
+module.exports.referAndEarn=async (req,res) =>{
     try{
         const {authorization}=req.headers
         let jwt=await verifyToken(authorization)
-        console.log('inside myRewards',jwt)
         SignupModel.findById(jwt.id,(err,data)=>{
             if(err) throw err
-            console.log(data.myInvites)
-            let referral=data.myInvites
-            let amount=data.myInvites.reduce((acc,res)=>{
-                acc=acc+res.amount
-                return acc
-            },0)
+            console.log('hello refer and earn',data)
             res.status(200).json({
-                totalAmount:amount,
-                referral
+                Url :`localhost:3000?refCode=${data.refCode}`,
+                refCode:data.refCode,
+                youGet: 30,
+                friendGets:50
             })
         })
-        
 
     }catch(err){
-        if(err) console.log('inside myRewards',err)
+        if(err) console.log('inside referAndEarn',err)
         res.status(400).json({
             status:'fail',
             message:'something went wrong',
--- route/main_route.js
+++ route/main_route.js
@@ -5,11 +5,13 @@ const {login} = require('../controller/login.js');
 const {myInvites} = require('../controller/myinvites.js');
 const {displayAll} = require('../controller/display.js');
 const {myRewards} = require('../controller/myrewards.js');
+const {referAndEarn} = require('../controller/referAndEarn.js');
 
 routes.get('/',displayAll)
 routes.post('/signup',signUp)
 routes.post('/login',login)
 routes.post('/myinvites',myInvites)
 routes.post('/myrewards',myRewards)
+routes.post('/refer_and_earn',referAndEarn)
 
 module.exports=routes
